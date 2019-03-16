4ef4f2532c8b8f7d459421f5a3d29c93c9aeacf1
Ankeet Maini <ankeet.maini@gmail.com>
2019-03-17 00:57:24 +0530

    Adds modal component

--- packages/modal/src/Modal.stories.tsx
+++ packages/modal/src/Modal.stories.tsx
@@ -2,8 +2,28 @@ import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import Modal from "./Modal";
 
-storiesOf("Modal", module).add("default", () => (
-  <Modal>
-    <h1>I am directly in body</h1>
-  </Modal>
-));
+storiesOf("Modal", module)
+  .add("default", () => (
+    <Modal>
+      <h1>I am the modal body</h1>
+    </Modal>
+  ))
+  .add("background visible", () => (
+    <div>
+      <h1>Ipsum Lorem</h1>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+      <Modal>
+        <h3>Hello, Modal!</h3>
+      </Modal>
+    </div>
+  ));
--- packages/modal/src/Modal.tsx
+++ packages/modal/src/Modal.tsx
@@ -1,23 +1,28 @@
-import React from "react";
+import * as React from "react";
 import Portal from "@hoppingfrog/portal";
-import { styled } from "linaria/react";
+import styled from "@emotion/styled";
 
 const Container = styled.div`
   position: relative;
 `;
 
 const Overlay = styled.div`
-  position: absolute;
+  position: fixed;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
-  background: black;
-  opacity: 0.2;
+  background: #e2e1e0;
+  opacity: 0.6;
 `;
 
 const Content = styled.div`
   position: absolute;
+  box-shadow: 0 3px 7px rgba(0, 0, 0, 0.3);
+  padding: 10px;
+  background: white;
+  border-radius: 10px;
+  border: 1px solid rgba(0, 0, 0, 0.3);
   top: 50%;
   left: 50%;
   transform: translateX(-50%) translateY(-50%);
